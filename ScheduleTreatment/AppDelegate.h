892c0b20e4fac23a43b532715d3ebb5804d6dcdf
wolf <wolf@wolfs-Mac.local>
2014-12-16 07:16:52 -0300

    Create project for use  parse framework

---
+++ ScheduleTreatment/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  ScheduleTreatment
+//
+//  Created by wolf on 12/15/14.
+//  Copyright (c) 2014 Z-Sys. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ ScheduleTreatment/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  ScheduleTreatment
+//
+//  Created by wolf on 12/15/14.
+//  Copyright (c) 2014 Z-Sys. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
