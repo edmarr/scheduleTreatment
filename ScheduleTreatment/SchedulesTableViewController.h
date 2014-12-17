75a7434047ec3f8063de6aa846631519d089d10c
wolf <wolf@wolfs-Mac.local>
2014-12-17 16:58:57 -0300

    Insert buttom for logout

--- ScheduleTreatment/SchedulesTableViewController.h
+++ ScheduleTreatment/SchedulesTableViewController.h
@@ -9,5 +9,7 @@
 #import <UIKit/UIKit.h>
 
 @interface SchedulesTableViewController : UITableViewController
+@property (nonatomic, strong) NSArray *list;
+- (IBAction)logout:(id)sender;
 
 @end
--- ScheduleTreatment/SchedulesTableViewController.m
+++ ScheduleTreatment/SchedulesTableViewController.m
@@ -7,6 +7,7 @@
 //
 
 #import "SchedulesTableViewController.h"
+#import <Parse/Parse.h>
 
 @interface SchedulesTableViewController ()
 
@@ -32,15 +33,11 @@
 #pragma mark - Table view data source
 
 - (NSInteger)numberOfSectionsInTableView:(UITableView *)tableView {
-#warning Potentially incomplete method implementation.
-    // Return the number of sections.
-    return 0;
+    return 1;
 }
 
 - (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section {
-#warning Incomplete method implementation.
-    // Return the number of rows in the section.
-    return 0;
+    return [self.list count];
 }
 
 /*
@@ -97,4 +94,8 @@
 }
 */
 
+- (IBAction)logout:(id)sender {
+    //[PFUser logOut];
+    //PFUser *currentUser = [PFUser currentUser];
+}
 @end
